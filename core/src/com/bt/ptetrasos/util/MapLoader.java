6e51600190902a1046bf36def38506a58c376c2e
Lukas Zapolskas <lukas.zapolskas@gmail.com>
2016-10-22 13:19:10 +0400

    Loading map

---
+++ core/src/com/bt/ptetrasos/util/MapLoader.java
@@ -0,0 +1,24 @@
+package com.bt.ptetrasos.util;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Created by Lukas on 22-Oct-16.
+ */
+public class MapLoader {
+
+    FileReader input = null;
+    String[] raw;
+    MapObject map;
+
+
+
+    public MapObject readFile(String file) throws IOException {
+        input = new FileReader(file);
+        raw = input.toString().split(",");
+        map = new MapObject(Float.parseFloat(raw[0]), Float.parseFloat(raw[1]), Float.parseFloat(raw[2]),
+        Float.parseFloat(raw[3]), Integer.getInteger(raw[4]), raw[5]) ;
+        return map;
+    }
+}
